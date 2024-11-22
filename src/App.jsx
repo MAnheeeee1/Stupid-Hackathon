2b7fef45de051115be3093d4eaca6c44da45ee03
Manh.D <07mandao@skola.boras.se>
2024-11-22 20:03:21 +0100

    inital comit, managed to import a working face recognition model

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useRef, useEffect } from "react";
+import "./App.css";
+import * as faceapi from "face-api.js";
+
+function App() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  // LOAD FROM USEEFFECT
+  useEffect(() => {
+    startVideo();
+    videoRef && loadModels();
+  }, []);
+
+  // OPEN YOU FACE WEBCAM
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((currentStream) => {
+        videoRef.current.srcObject = currentStream;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  // LOAD MODELS FROM FACE API
+
+  const loadModels = () => {
+    Promise.all([
+      // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+    ]).then(() => {
+      faceMyDetect();
+    });
+  };
+
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+      if (detections.length > 0) {
+        detections.forEach((detection) => {
+          const expressions = detection.expressions;
+          if (expressions.angry > 0.5) {
+            console.log("Angry");
+          }
+        });
+      }
+    }, 1000);
+  };
+
+  return (
+    <div className="myapp">
+      <h1>FAce Detection</h1>
+      <div className="appvide">
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+      </div>
+    </div>
+  );
+}
+
+export default App;
